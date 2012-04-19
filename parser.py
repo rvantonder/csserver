1291aa739b6b9da2c57150789cd162be2c932616
Rai <rvantonder@gmail.com>
2012-04-19 03:46:23 -0500

    modified parser

--- parser.py
+++ parser.py
@@ -1,4 +1,26 @@
+import re
+
+char_to_digit = {"l" : "1", 
+                 "T" : "1", 
+                 "I" : "1", 
+                 "0" : "0", 
+                 "1" : "1", 
+                 "2" : "2", 
+                 "3" : "3", 
+                 "4" : "4", 
+                 "5" : "5", 
+                 "6" : "6", 
+                 "7" : "7", 
+                 "8" : "8", 
+                 "9" : "9",
+                 "." : "."}
+
 def process_price(v):
+  """
+  In: string value such as '3T.99'
+  Out: Common mistakes corrected, such as character T -> 1
+  """
+
   s = None
 
   try:
@@ -9,19 +31,22 @@ def process_price(v):
   return s
 
 def parse(filename):
+  """
+  In: filename
+  Out: dictionary with parsed values (text)
+  """
 
   f = open(filename, 'r')
 
   d = {}
 
-  #pattern = re.compile("R*\.)")
-
   for line in f.readlines():
+    #matches something like: "blah blah R(31.99)<ignored>"
     m = re.match(".*R(\w+\.\w+)", line) #relient on matching digits
     if m: 
-      print m.group(0)
+      #print m.group(0) #entire line
       item = ' '.join(m.group(0).split(" ")[:-1])
-      val = process_price(m.group(1))
+      val = process_price(m.group(1)) #converts common OCR number 'mistakes'
       d[item] = val 
 
   return d
